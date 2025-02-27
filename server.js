d96d40534fff302e8b726939be4c37ff873fe5e5
praveen453 <pjayakopdy524@gmail.com>
2025-02-27 19:34:55 +0530

    update

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ app.use('/api/reminders', reminderRoutes);
 
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
 }).then(() => console.log('MongoDB Connected'))
-.catch(err => console.error(err));
+
 
 app.get('/', (req, res) => {
 res.send('Break Reminder API Running...');
